f0b0f4f56d4780ea07743ff0c4ba4710d2bef7be
unknown <mellolefloth@gmail.com>
2020-07-13 01:33:22 -0300

    Se fixeó la cantidad de tarjetas para mostrar + Validaciones + Fixeo en situaciones de no encontrar peliculas

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,49 +1,67 @@
 import React,{useState} from 'react';
 import './Home.scss';
-
+import Swal from 'sweetalert2'
 
 
 /* Se cambio el class por function, me siento más comodo armando componentes con funciones*/
 
 function Home(){
 
- var API_KEY= '9d4c9018'
- //var url= 'http://img.omdbapi.com/?apikey=[yourkey]&'
+ const API_KEY= '9d4c9018'
 
- const [searchResults, setSearchResults]= useState([]) 
+ const [movies, setMovies]= useState([]) 
  const [userInput, setUserInput] = useState('')
- const[title, setTitle]= useState('')
- const [poster, setPoster] = useState(null)
- const [year, setYear] = useState('')
  const [imdbID,setImdbID] =useState('') 
+ const [thereIsResult, setThereIsResult] = useState(false)
+ 
 
 /*Este es el evento que tomará los datos de la pelicula que ingresara el usuario*/
 
 const handleChange = event => {
   setUserInput(event.target.value)
-  //alert('Este es el INPUT' + setUserInput(event.target.value))
+  
 }
 
 const handleSubmit = event => {
   event.preventDefault()
+  
+  
   fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${userInput}`).then(
     resp => {
-      // resp es la respuesta que obtuvimos del servidor pero asi no nos sirve de mucho, lo que debemos hacer es darle el formato adecuado con el metdo json() el cual devulve otra promesa con la respuesta ya formateada a como la deseamos
+      // Se convierte la respuesta a json para que sea maleable la info. Se podrá mostrar.
       resp.json().then(data => {
-        data.Search && setSearchResults(data.Search)
-        data.Search[0].Title && setTitle(data.Search[0].Title)
-        data.Search[0].Poster && setPoster(data.Search[0].Poster)
-        data.Search[0].Year && setYear(data.Search[0].Year)
-        data.Search[0].imdbID && setImdbID(data.Search[0].imdbID)
+      
+        try{
+          
+          if(data.Search){
+          setMovies(data.Search)
+          setThereIsResult(true)
+          }else{ // EN el caso de que conteste, pero no existan pelis. limpia
+            setThereIsResult(false)
+            Swal.fire({
+              text: 'No se encuentra esta pelicula en el Catálogo',
+              icon: 'error'
+             })
+          }
+        }catch(e){ //responde un 500 
+          Swal.fire({
+           text: 'No se encuentra esta pelicula en el Catálogo',
+           icon: 'error'
+          })
+          setThereIsResult(false)
+        }
+
+
+
       })
     }
   )
 }
 
 
-
   const imdbLink = `https://www.imdb.com/title/${imdbID}`
   return (
+  <>
     <div className="App">
       <header>
         <h1><span>// </span> React challenge</h1>
@@ -62,18 +80,37 @@ const handleSubmit = event => {
           <button type="submit">Search</button>
         </form>
 
-         <div className="results">
-          <article>
-            <h3>{title}</h3>
-            <img src={poster} alt='Card image cap'/>
-            <p>{year} - <a href={imdbLink}>VIEW ON IMDB</a></p>
-          </article>
-        </div> 
+      <div className="results">
+          {/* <articleComponent title={title}
+                            poster={poster}
+                            year={year}
+                            imdbLink={imdbLink}
+        /> */}
+        {thereIsResult == true && movies != null ? (
+
+        movies.map( movie =>{
+          return(
+                <article>
+                <h3>{movie.Title}</h3>
+                <img src={movie.Poster} alt='Card image cap'/>
+                <p>{movie.Year} - <a href={`https://www.imdb.com/title/${movie.imdbID}`}>VIEW ON IMDB</a></p>
+               </article>
+                  )
+            }
+        )
+      ) : (
+       null
+      )}
+       
+      </div> 
       </main>
     </div>
+  </>
   );
 
 }
 
 
 export default Home;
+
+ 
